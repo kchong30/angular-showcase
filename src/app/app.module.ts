09febeba3fea70906739b55e0402e1520214ee90
Kevin Chong <kchong30@my.bcit.ca>
2023-03-01 10:31:47 -0800

    Pipe for tags filter added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { ProjectFilterPipe } from './project-filter.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, ProjectFilterPipe],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
